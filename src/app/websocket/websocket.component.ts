43a3515edfd04cbba608aa100d0008fcacdaaf5b
Robert Rosentrater <rorosentrater@hotmail.com>
2021-06-12 11:14:52 -0400

    Made a simple websocket service. backend needed some changes so I will probably make the backend repo for this proj soon

--- src/app/websocket/websocket.component.ts
+++ src/app/websocket/websocket.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SimpleWebsocketService } from '../services/simpleWebsocket/simple-websocket.service'
+
 
 @Component({
   selector: 'app-websocket',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsocketComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SimpleWebsocketService) { }
 
   ngOnInit(): void {
+    this.service.messages$.subscribe(
+      msg => {
+        console.log('Message from server:', msg)
+        // console.log(typeof msg)
+        // msg = JSON.parse(msg)
+        // console.log('Message from server parsed:', msg)
+        // console.log(typeof msg)
+      },
+      error => {
+        console.log('Error on socket connection:', error)
+      },
+      () => {
+        console.log('this is a complete or something idk. Connection was closed?')
+      }
+    )
+    this.service.sendMessage({"action": "whatever", "message": {'key': 'val'}})
+    this.service.sendMessage({"action": "whatever", "message": 'simple string message'})
   }
 
 }
